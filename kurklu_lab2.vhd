6d715ad82d4d25be7039535933d2fc118b655ec1
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-11 14:37:45 +0100

    ajout des autres 7 segments

--- kurklu_lab2.vhd
+++ kurklu_lab2.vhd
@@ -6,7 +6,9 @@ entity kurklu_lab2 is
 	port(
 		clk : in std_logic := '0';
 		reset_n : in std_logic :='0';
-		segment1 : out std_logic_vector(6 downto 0));
+		segment1 : out std_logic_vector(6 downto 0);
+		segment2 : out std_logic_vector(6 downto 0);
+		segment3 : out std_logic_vector(6 downto 0));
 		
 end entity kurklu_lab2;
 
@@ -16,8 +18,10 @@ architecture rit of kurklu_lab2 is
 	component kurklu_lab2_sys is
         port (
             clk_clk                         : in  std_logic                    := 'X'; -- clk
-            reset_reset_n                   : in  std_logic                    := 'X'; -- reset_n      
-            seg1_external_connection_export : out std_logic_vector(3 downto 0)         -- export
+            reset_reset_n                   : in  std_logic                    := 'X'; -- reset_n
+            seg1_external_connection_export : out std_logic_vector(3 downto 0);        -- export
+            seg2_external_connection_export : out std_logic_vector(3 downto 0);        -- export
+            seg3_external_connection_export : out std_logic_vector(3 downto 0)         -- export
         );
     end component kurklu_lab2_sys;
 	 
@@ -28,7 +32,9 @@ architecture rit of kurklu_lab2 is
 			
 	end component bin_to_7seg;
 
-signal tmp : std_logic_vector(3 downto 0);
+signal tmp1 : std_logic_vector(3 downto 0);
+signal tmp2 : std_logic_vector(3 downto 0);
+signal tmp3 : std_logic_vector(3 downto 0);
 begin
 
 
@@ -37,15 +43,30 @@ begin
         port map (
             clk_clk                         => clk,                         --                      clk.clk
             reset_reset_n                   => reset_n,                   --                    reset.reset_n
-            seg1_external_connection_export => tmp  -- seg1_external_connection.export
-        );
+            seg1_external_connection_export => tmp1,
+				seg2_external_connection_export => tmp2, 
+            seg3_external_connection_export => tmp3);
 		  
 
-	convert : component bin_to_7seg
+	convert1 : component bin_to_7seg
 			port map (
-				input => tmp,
+				input => tmp1,
 				output => segment1);
+				
+				
+	convert2 : component bin_to_7seg
+			port map (
+				input => tmp2,
+				output => segment2);
+
+	convert3 : component bin_to_7seg
+			port map (
+				input => tmp3,
+				output => segment3);
 		  
 end architecture;
+
+
+
 
 
