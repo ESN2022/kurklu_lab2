42546358a1f0c8d78859ca3840eda03a3d1122c7
Ahmet KÜRKLÜ <ahmetkurklu2000@gmail.com>
2023-01-11 12:23:09 +0100

    Modification bin_to_7seg integer -> bcd

--- bin_to_7seg.vhd
+++ bin_to_7seg.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity bin_to_7seg is
 	port(
-		input : in integer range 0 to 9;
+		input : in std_logic_vector(3 downto 0);
 		output : out std_logic_vector(6 downto 0));
 		
 end entity bin_to_7seg;
@@ -13,15 +13,15 @@ architecture rit of bin_to_7seg is
 
 begin
 
-	output <=	"1000000" when input = 0 else 
-					"1111001" when input = 1 else 
-					"0100100" when input = 2 else 
-					"0110000" when input = 3 else 
-					"0011001" when input = 4 else 
-					"0010010" when input = 5 else 
-					"0000010" when input = 6 else 
-					"1111000" when input = 7 else 
-					"0000000" when input = 8 else 
-					"0010000" when input = 9;
+	output <=	"1000000" when input = "0000" else 
+					"1111001" when input = "1001" else 
+					"0100100" when input = "0010" else 
+					"0110000" when input = "0011" else 
+					"0011001" when input = "0100" else 
+					"0010010" when input = "0101" else 
+					"0000010" when input = "0110" else 
+					"1111000" when input = "0111" else 
+					"0000000" when input = "1000" else 
+					"0010000" when input = "1001";
 		  
 end architecture;
